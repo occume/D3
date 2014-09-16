9eee0be5bf748e8e45c017f2341225a30cae2716
d_jin <d_jin@ctrip.com>
2014-09-16 17:42:19 +0800

    增加 printer 方法

---
+++ D3-Std/src/main/java/org/d3/std/Printer.java
@@ -0,0 +1,54 @@
+package org.d3.std;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.common.collect.Maps;
+
+
+public class Printer {
+	
+	public static void printObject(Object add) throws Exception{
+		
+		Map<String, String> mnames = Maps.newHashMap();
+		int maxLen = -1;
+		
+		Method[] methods = add.getClass().getMethods();
+		for(Method m: methods){
+			if(!m.isVarArgs()){
+				Object ret = null;
+				try{
+					ret = m.invoke(add);
+				}
+				catch(Throwable e){
+					ret = "ERR";
+				}
+				if(m.getName().length() > maxLen){
+					maxLen = m.getName().length();
+				}
+				mnames.put(m.getName(), String.valueOf(ret));
+			}
+		}
+		
+		for(String key: mnames.keySet()){
+			int dis = maxLen - key.length();
+			StringBuilder b = new StringBuilder();
+			for(int i = 0; i < dis; i++){
+				b.append(" ");
+			}
+			b.append(key)
+			 .append(" : ")
+			 .append(mnames.get(key));
+			System.out.println(b);
+		}
+		System.out.println("-------------------------------------------");
+	}
+	
+	public static void printMap(Map map){
+		for(Object key: map.keySet()){
+			System.out.println(key + " : " + map.get(key));
+		}
+	}
+	
+}
